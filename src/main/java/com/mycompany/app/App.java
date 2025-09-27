5f4700597bfeafc712b6df8fc077035d79f1dec5
Trived93 <trivedsaigurram93@gmail.com>
2025-09-27 16:40:49 +0530

    Gurram Trived Sai

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Trived Sai Gurram";
+    private static final String MESSAGE = "Trived Sai ";
 
     public App() {}
 
