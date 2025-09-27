44ef279f9dbe79c5b46b5ecce4e13ebc389a9979
Trived93 <trivedsaigurram93@gmail.com>
2025-09-27 17:02:01 +0530

    Vishank added

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Trived Sai ";
+    private static final String MESSAGE = " Vishank Battula ";
 
     public App() {}
 
